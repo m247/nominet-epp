d0a96e806bbd596272c0a1c7a3f4b9de1aef7fa2
Geoff Garside <geoff@geoffgarside.co.uk>
2010-05-21 14:45:42 +0100

    Add account and contact node wrappers

--- lib/nominet-epp.rb
+++ lib/nominet-epp.rb
@@ -37,12 +37,10 @@ module NominetEPP
       def data_namespaces(data)
         data.namespaces.definitions.map(&:to_s)
       end
-      def domain(node_name, &block)
-        node = XML::Node.new(node_name)
-        node.namespaces.namespace =
-          XML::Namespace.new(node, 'domain', 'http://www.nominet.org.uk/epp/xml/nom-domain-2.0')
-        node['xsi:schemaLocation'] =
-          "http://www.nominet.org.uk/epp/xml/nom-domain-2.0 nom-domain-2.0.xsd"
+      def new_node(name, ns_prefix, ns_href, schema_uri, &block)
+        node = XML::Node.new(name)
+        node.namespaces.namespace = XML::Namespace.new(node, ns_prefix, ns_uri)
+        node['xsi:schemaLocation'] = schema_uri
 
         case block.arity
         when 0
@@ -54,6 +52,18 @@ module NominetEPP
         end
 
         node
+      end
+      def domain(node_name, &block)
+        new_node(node_name, 'domain', 'http://www.nominet.org.uk/epp/xml/nom-domain-2.0',
+          "http://www.nominet.org.uk/epp/xml/nom-domain-2.0 nom-domain-2.0.xsd", &block)
+      end
+      def account(node_name, &block)
+        new_node(node_name, 'account', 'http://www.nominet.org.uk/epp/xml/nom-account-2.0',
+          "http://www.nominet.org.uk/epp/xml/nom-account-2.0 nom-account-2.0.xsd", &block)
+      end
+      def contact(node_name, &block)
+        new_node(node_name, 'contact', 'http://www.nominet.org.uk/epp/xml/nom-contact-2.0',
+          "http://www.nominet.org.uk/epp/xml/nom-contact-2.0 nom-contact-1.0.xsd", &block)
       end
   end
 end
