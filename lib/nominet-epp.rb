de214e61cb4fcb0c56cfdd6c4a792d5131573a05
Geoff Garside <geoff@geoffgarside.co.uk>
2010-05-21 12:59:53 +0100

    Add unrenew operation support

--- lib/nominet-epp.rb
+++ lib/nominet-epp.rb
@@ -80,6 +80,25 @@ module NominetEPP
       raise "Renewed name #{renName} does not match #{name}" if renName != name
       return Time.parse(renExp)
     end
+    def unrenew(*names)
+      resp = @client.unrenew do
+        domain('unrenew') do |node, ns|
+          names.each do |name|
+            node << XML::Node.new('name', name, ns)
+          end
+        end
+      end
+
+      return false unless resp.success?
+
+      hash = {}
+      resp.data.find('//domain:renData', data_namespaces(resp.data)).each do |node|
+        renName = node.find('domain:name', data_namespaces(resp.data)).first.content.strip
+        renExp  = node.find('domain:exDate', data_namespaces(resp.data)).first.content.strip
+        hash[renName] = Time.parse(renExp)
+      end
+      hash
+    end
     def delete(name)
       resp = @client.delete do
         domain('delete') do |node, ns|
