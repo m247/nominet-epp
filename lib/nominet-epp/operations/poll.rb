4fcb7bc5315c032761f5b8b328332256ad7ab617
Geoff Garside <geoff@geoffgarside.co.uk>
2011-01-17 11:06:37 +0000

    Tweak how #poll method works w.r.t blocks

--- lib/nominet-epp/operations/poll.rb
+++ lib/nominet-epp/operations/poll.rb
@@ -2,23 +2,41 @@ module NominetEPP
   module Operations
     # EPP Poll Operation
     module Poll
-      # Poll the EPP server for events
+      class AckError < RuntimeError; end
+
+      # Poll the EPP server for events.
+      #
+      # If a block is given then it will be invoked once for
+      # each pending event. If no block is given the only the
+      # first received event will be returned along with the
+      # message ID of the event to allow the message to be
+      # ack'd. nil is returned if there is an error in the
+      # response or if there are not further messages to process.
+      #
+      # @example Without a block
+      #   msgID, xml = client.poll
+      #   ... process xml ...
+      #   client.ack(msgID)
+      #
+      # @example With a block
+      #   client.poll do |xml|
+      #     ... process xml ...
+      #   end
       #
       # @yield [data] process data if messages to poll
       # @yieldparam [XML::Node] data Response data
       # @return [nil] no messages to handle
-      # @return [Boolean] ack successful
+      # @return [Array<String,XML::Node>] message ID and response xml data
+      # @raise [AckError] ack of event notification failed
       # @see ack
       def poll
-        resp = @client.poll do |poll|
-          poll['op'] = 'req'
+        while resp = poll_req
+          return if resp.code != 1301 || resp.msgQ['count'] == '0'
+          return [resp.msgQ['id'], resp.data] unless block_given?
+
+          yield resp.data
+          raise AckError, "failed to acknowledge message #{resp.msgQ['id']}" unless ack(resp.msgQ['id'])
         end
-
-        return if resp.code != 1301 || resp.msgQ['count'] == '0'
-
-        yield resp.data
-
-        ack(resp.msgQ['id'])
       end
 
       # Acknowledges a polled message ID
@@ -33,6 +51,13 @@ module NominetEPP
 
         return resp.success?
       end
+
+      protected
+        def poll_req
+          @client.poll do |poll|
+            poll['op'] = 'req'
+          end
+        end
     end
   end
 end
