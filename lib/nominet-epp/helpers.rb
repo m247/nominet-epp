c7aba7dcfdda7cb8d822c6a081763ba441cd8bf7
Geoff Garside <geoff@geoffgarside.co.uk>
2011-05-04 10:40:45 +0100

    Refactor account_fields_xml to use new _to_xml helpers. Correct element ordering in generated XML as well.

--- lib/nominet-epp/helpers.rb
+++ lib/nominet-epp/helpers.rb
@@ -54,28 +54,17 @@ module NominetEPP
     # @param [XML::Node] node XML Node to add the attributes to
     # @param [XML::Namespace] ns XML Namespace to create the elements under
     def account_fields_xml(fields, node, ns)
-      fields.each do |k,v|
+      [:trad_name, :type, :co_no, :opt_out, :addr, :contacts].each do |k|
+        next if fields[k].nil?
         case k
         when :contacts
-          v.each do |c|
-            node << (c_node = XML::Node.new('contact', nil, ns))
-            c_node['order'] = c.delete(:order)
-            c_node['type'] = 'admin'
-
-            c_node << contact(c.delete(:action) || 'create') do |cn, cns|
-              c.each do |l,w|
-                cn << XML::Node.new(l, w, cns)
-              end
-            end
+          account_contacts_to_xml(fields[k], ns) do |n|
+            node << n
           end
         when :addr  # Limitation, can only handle one addr at a time
-          node << (a_node = XML::Node.new('addr', nil, ns))
-          a_node['type'] = 'admin'
-          v.each do |l,w|
-            a_node << XML::Node.new(l, w, ns)
-          end
+          node << addr_to_xml(fields[k], ns)
         else
-          node << XML::Node.new(k.to_s.gsub('_', '-'), v, ns)
+          node << generic_field_to_xml(k, fields[k], ns)
         end
       end
     end
