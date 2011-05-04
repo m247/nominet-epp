944f4ed5f0d82dcc099fb979ae14755b94cd6af0
Geoff Garside <geoff@geoffgarside.co.uk>
2011-05-04 10:39:12 +0100

    Add helper methods for correctly converting addresses, contacts, accounts and account contacts to their EPP formats with the correct sequenced order required by the XML schema

--- lib/nominet-epp/helpers.rb
+++ lib/nominet-epp/helpers.rb
@@ -79,5 +79,91 @@ module NominetEPP
         end
       end
     end
+
+    # Creates an XML node with the dashed form of the +name+.
+    #
+    # @param [String] name Element name, underscores will be converted to hyphens
+    # @param [String] value Element value
+    # @param [XML::Namespace] ns XML Namespace to create the element under
+    # @return [XML::Node]
+    def generic_field_to_xml(name, value, ns)
+      XML::Node.new(name.to_s.gsub('_', '-'), value, ns)
+    end
+
+    # Creates and array of XML::Node objects for each contact passed.
+    #
+    # @param [Array] contacts Array of contacts
+    # @param [XML::Namespace] ns XML Namespace to create the elements under
+    # @yield [node]
+    # @yieldparam [XML::Node] node XML contact element
+    # @return [Array] array of XML contact nodes
+    def account_contacts_to_xml(contacts, ns)
+      raise ArgumentError, "contacts must be an Array" unless contacts.is_a?(Array)
+      raise ArgumentError, "ns must be an xml namespace" unless ns.is_a?(XML::Namespace)
+
+      contacts.map do |contact|
+        account_contact_to_xml(contact, ns).tap |n|
+          yield n if block_given?
+        end
+      end
+    end
+
+    # Creates an XML contact element
+    #
+    # @param [Hash] contact Contact attributes
+    # @param [XML::Namespace] ns XML Namespace to create the elements under
+    # @return [XML::Node] XML contact element
+    def account_contact_to_xml(contact, ns)
+      raise ArgumentError, "contact must be a hash" unless contact.is_a?(Hash)
+      raise ArgumentError, "ns must be an xml namespace" unless ns.is_a?(XML::Namespace)
+
+      XML::Node.new('contact', nil, ns).tap do |node|
+        node['order'] = contact.delete(:order) if contact.has_key?(:order)
+        node['type'] = 'admin'
+
+        node << contact_to_xml(contact)
+      end
+    end
+
+    # Creates an XML +contact:create+ or +contact:update+ element.
+    #
+    # The choice to create a +create+ or +update+ element is determined by the presence of
+    # a +:roid+ key in the list of attributes.
+    #
+    # @param [Hash] contact Contact attributes
+    # @return [XML::Node] XML contact:+action+ element with the attributes
+    def contact_to_xml(contact)
+      keys = [:roid, :name, :phone, :mobile, :email]
+      raise ArgumentError, "contact must be a hash" unless contact.is_a?(Hash)
+      raise ArgumentError, "Contact allowed keys are #{keys.join(', ')}" unless (contact.keys - keys).empty?
+      raise ArgumentError, "Contact requires name and email keys" unless contact.has_key?(:name) && contact.has_key?(:email)
+
+      action = contact[:roid].nil? || contact[:roid] == '' ? 'create' : 'update'
+      contact(action) do |node, ns|
+        keys.each do |key|
+          next if contact[key].nil? || contact[key] == ''
+          node << XML::Node.new(key, contact[key], ns)
+        end
+      end
+    end
+
+    # Creates an XML addr element
+    #
+    # @param [Hash] addr Address attributes
+    # @param [XML::Namespace] ns XML Namespace to create the elements in
+    # @return [XML::Node] XML addr element
+    def addr_to_xml(addr, ns)
+      keys = [:street, :locality, :city, :county, :postcode, :country]
+      raise ArgumentError, "addr must be a hash" unless addr.is_a?(Hash)
+      raise ArgumentError, "ns must be an xml namespace" unless ns.is_a?(XML::Namespace)
+      raise ArgumentError, "Address allowed keys are #{keys.join(', ')}" unless (addr.keys - keys).empty?
+
+      XML::Node.new('addr', nil, ns).tap do |a|
+        keys.each do |key|
+          next if addr[key].nil? || addr[key] == ''
+          a << XML::Node.new(key, addr[key], ns)
+        end
+      end
+    end
   end
 end
