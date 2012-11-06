ce7c8884f82fccca9c1f3eb4485c1a7164a48f08
Geoff Garside <geoff@geoffgarside.co.uk>
2012-11-06 14:39:32 +0000

    Add instrumentation to :check operation.
    The following instruments are emitted:
    
      * nominet-epp.check
      * nominet-epp.check.request
      * nominet-epp.check.parse
    
    the first instrument covers the entire operation while the request and
    parse operations are scoped to just the network and XML processing
    sections of the operation.

--- lib/nominet-epp/operations/check.rb
+++ lib/nominet-epp/operations/check.rb
@@ -4,29 +4,60 @@ module NominetEPP
     module Check
       # Check the availablity of one or more domain names
       #
+      # @param [Symbol] entity Type of entity to check, :domain, :contact or :host
       # @param [String, ...] names List of names to check
       # @return [false] request failed
       # @return [true] the domain name is available
       # @return [Hash<String,Boolean>] availability by domain name
-      def check(*names)
-        resp = @client.check do
-          domain('check') do |node, ns|
-            names.each do |name|
-              node << XML::Node.new('name', name, ns)
+      def check(entity, *names)
+        raise "Unsupported entity #{entity}" unless [:domain, :contact, :host].include?(entity)
+
+        instrument :check, :entity => entity, :names => names do
+          resp = instrument 'check.request', :entity => entity do
+            @client.check do
+              case entity
+              when :domain
+                domain('check') do |node, ns|
+                  names.each do |name|
+                    node << XML::Node.new('name', name, ns)
+                  end
+                end
+              when :contact
+                contact('check') do |node, ns|
+                  names.each do |id|
+                    node << XML::Node.new('id', id, ns)
+                  end
+                end
+              when :host
+                host('check') do |node, ns|
+                  names.each do |name|
+                    node << XML::Node.new('name', name, ns)
+                  end
+                end
+              end
             end
           end
-        end
 
-        return false unless resp.success?
+          return false unless resp.success?
 
-        @check_limit = check_abuse_limit(resp.extension)
-        results = resp.data.find('//domain:name', namespaces)
-        if results.size > 1
-          hash = {}
-          results.each {|node| hash[node.content.strip] = (node['avail'] == '1') }
-          hash
-        else
-          results.first['avail'] == '1'
+          if entity == :domain
+            @check_limit = check_abuse_limit(resp.extension)
+          end
+
+          xpath = case entity
+          when :domain then '//domain:name'
+          when :contact then '//contact:id'
+          when :host then '//host:name'
+          end
+
+          instrument 'check.parse', :entity => entity do
+            result = resp.data.find(xpath, namespaces).inject({}) do |hash, node|
+              hash[node.content.strip] = node['avail'] == '1'
+              hash
+            end
+
+            result.size > 1 ? result : result[0]
+          end
         end
       end
 
