6f5744b3dc136b589830726b2f05257260de29f4
Geoff Garside <geoff@geoffgarside.co.uk>
2012-11-06 14:53:08 +0000

    Update create to support standard-epp.
    Additionally supports instrumentation with the following keys
    
      * nominet-epp.create
      * nominet-epp.create.request
      * nominet-epp.create.parse

--- lib/nominet-epp/operations/create.rb
+++ lib/nominet-epp/operations/create.rb
@@ -1,137 +1,231 @@
+require 'securerandom'
 module NominetEPP
   module Operations
     # EPP Create Operation
     module Create
-      # Register a domain name with Nominet.
-      #
-      # The returned hash has the following keys
-      # - (String) +:name+ --    Domain name registered
-      # - (Time) +:crDate+ --    Domain creation date
-      # - (Time) +:exDate+ --    Domain expiration date
-      # - (Hash) +:account+ --   Created account data (if returned)
-      #
-      # The +:account+ hash contains the following keys
-      # - (String) +:roid+ --    Account ID
-      # - (String) +:name+ --    Account Name
-      # - (Array) +:contacts+ -- Account contacts
-      #
-      # The +:contacts+ array contains hashes of the following keys
-      # - (String) +:roid+ --    Contact ID
-      # - (String) +:name+ --    Contact Name
-      # - (String) +:type+ --    Contact type if set
-      # - (Integer) +:order+ --  Contacts order in the list of contacts
-      #
-      # @param [String] name Domain name to register
-      # @param [String, Hash] acct
-      # @param [String, Array] nameservers Nameservers to set for the domain
-      # @param [Hash] options Registration options
-      # @option options [String] :period
-      # @option options [String] :first_bill
-      # @option options [String] :recur_bill
-      # @option options [String] :auto_bill
-      # @option options [String] :next_bill
-      # @option options [String] :notes
-      # @raise [ArgumentError] invalid option keys
-      # @return [false] registration failed
-      # @return [Hash] Domain creation data
-      def create(name, acct, nameservers, options = {})
-        raise ArgumentError, "options allowed keys are period, first_bill, recur_bill, auto_bill, next_bill, notes" unless (options.keys - [:period, :first_bill, :recur_bill, :auto_bill, :next_bill, :notes]).empty?
+      # List of update keys for domains which are sent on the <epp:extension> element
+      # @internal These must be kept in the order defined by the XML Schema
+      DOMAIN_EXTENSION_KEYS = [:first_bill, :recur_bill, :auto_bill, :next_bill, :auto_period, :next_period, :notes, :reseller]
 
-        resp = @client.create do
-          domain('create') do |node, ns|
-            node << XML::Node.new('name', name, ns)
+      # List of update keys for contacts which are sent on the <epp:extension> element
+      # @internal These must be kept in the order defined by the XML Schema
+      CONTACT_EXTENSION_KEYS = [:trad_name, :type, :co_no, :opt_out]
 
-            if period = options.delete(:period)
-              unit = period[-1..1]
-              num = period.to_i.to_s
-              p = XML::Node.new('period', num, ns);
-              p['unit'] = unit
-              node << p
-            end
+      # @param [Symbol] entity Type of object to create
+      # @param [String] name Name of the object to create
+      # @param [Hash] options Options/attributes of the entity to create
+      def create(entity, name, options = {})
+        create_method = :"create_#{entity}"
 
-            # node << XML::Node.new('auto-period', '', ns) # placeholder
+        raise ArgumentError, "entity #{entity} is not supported" unless self.respond_to?(create_method, true)
 
-            node << XML::Node.new('account', nil, ns).tap do |acct_xml|
-              acct_xml << create_account(acct, ns)
-            end
-
-            node << domain_ns_xml(nameservers, ns)
-
-            # Enforce correct sequencing of option fields
-            [:first_bill, :recur_bill, :auto_bill, :next_bill, :notes].each do |key|
-              next if options[key].nil? || options[key] == ''
-              node << XML::Node.new(key.to_s.gsub('_', '-'), options[key], ns)
+        instrument :create, :entity => entity, :name => name do
+          resp = instrument 'create.request', :entity => entity do
+            @client.create do |create, extension|
+              self.send(create_method, create, extension, name, options)
             end
           end
-        end
 
-        unless resp.success?
-          @error_info = {
-            :name => node_value(resp.data, '//domain:failData/domain:name/node()'),
-            :reason => node_value(resp.data, '//domain:failData/domain:reason/node()') }
+          if resp.success?
+            return instrument 'create.parse', :entity => entity do
+              self.send(:"create_#{entity}_response", resp)
+            end
+          end
+
+          # Capture error information
+          @error_info = { :name => resp.message, :reason => resp.error_reason }
           return false
         end
-
-        creData = resp.data.find('//domain:creData', namespaces).first
-        h = { :name => node_value(creData, 'domain:name'),
-          :crDate => Time.parse(node_value(creData, 'domain:crDate')),
-          :exDate => Time.parse(node_value(creData, 'domain:exDate')) }
-        unless creData.find('domain:account').first.nil?
-          h[:account] = created_account(creData.find('domain:account/account:creData', namespaces).first)
-        end
-        h
       end
-      
+
       private
-        # Create the account payload
+        # :name, :period, :ns, :registrant
         #
-        # @param [String, Hash] acct Account ID or New account fields
-        # @param [XML::Namespace] domain_ns +:domain+ namespace
-        # @raise [ArgumentError] acct must be a String or a Hash of account fields
-        # @return [XML::Node]
-        def create_account(acct, domain_ns)
-          if acct.kind_of?(String)
-            XML::Node.new('account-id', acct, domain_ns)
-          elsif acct.kind_of?(Hash)
-            account('create') do |node, ns|
-              node << XML::Node.new('name', acct[:name], ns)
-              account_fields_xml(acct, node, ns)
+        # Nominet declined
+        #  :contact
+        # Nominet ignored
+        #  :authInfo (EPP required, so SecureRandom.hex(8) if missing)
+        # 
+        def create_domain(create, extension, name, options)
+          extensions = options.dup
+          extensions.delete_if { |k,_| !DOMAIN_EXTENSION_KEYS.include?(k) }
+          options.delete_if { |k,_| DOMAIN_EXTENSION_KEYS.include?(k) }
+
+          create << domain('create') do |node, ns|
+            node << XML::Node.new('name', name, ns)
+
+            options[:auth_info] ||= options.delete(:authInfo) || SecureRandom.hex(8).upcase
+            options.delete(:authInfo) # Make sure its not present
+
+            # Sequence of elements must be in this order
+            [:period, :ns, :registrant, :contact, :auth_info].each do |key|
+              value = options[key]
+              next if value.nil?
+
+              case key
+              when :period
+                unit, period = value[-1..1], value.to_i.to_s
+                node << XML::Node.new('period', period, ns).tap do |n|
+                  n['unit'] = unit
+                end
+              when :ns
+                node << XML::Node.new('ns', nil, ns).tap do |n|
+                  value.each do |nameserver|
+                    n << XML::Node.new('hostObj', nameserver, ns)
+                  end
+                end
+              when :registrant
+                node << XML::Node.new('registrant', value, ns)
+              when :auth_info
+                node << XML::Node.new('authInfo', nil, ns).tap do |n|
+                  n << XML::Node.new('pw', value, ns)
+                end
+              end
             end
-          else
-            raise ArgumentError, "acct must be String or Hash"
+          end
+
+          unless extensions.empty?
+            extension << domain_ext('create') do |node, ns|
+              DOMAIN_EXTENSION_KEYS.each do |key|
+                value = extensions[key]
+                next if value.nil?
+
+                case key
+                when :notes
+                  Array(value).each do |note|
+                    node << XML::Node.new('notes', note, ns)
+                  end
+                else
+                  name = key.to_s.gsub('_','-')
+                  node << XML::Node.new(name, value, ns)
+                end
+              end
+            end
           end
         end
 
-        # Collects created account information
-        #
-        # @param [XML::Node] creData XML
-        # @return [Hash]
-        def created_account(creData)
-          { :roid => node_value(creData, 'account:roid'),
-            :name => node_value(creData, 'account:name'),
-            :contacts => created_contacts(creData.find(
-              'account:contact', namespaces)) }
+        def create_domain_response(resp)
+          creData = resp.data.find('//domain:creData', namespaces).first
+          { :name => node_value(creData, 'domain:name'),
+            :crDate => Time.parse(node_value(creData, 'domain:crDate')),
+            :exDate => Time.parse(node_value(creData, 'domain:exDate')) }
         end
 
-        # Collects created account contacts
-        #
-        # @param [XML::Node] account_contacts Account contacts
-        # @return [Hash]
-        def created_contacts(account_contacts)
-          account_contacts.map do |cont|
-            { :type => cont['type'],
-              :order => cont['order'] }.merge(
-                created_contact(cont.find('contact:creData', namespaces).first))
+
+        def create_contact(create, extension, name, options)
+          extensions = options.dup
+          extensions.delete_if { |k,_| !CONTACT_EXTENSION_KEYS.include?(k) }
+          options.delete_if { |k,_| CONTACT_EXTENSION_KEYS.include?(k) }
+
+          create << contact('create') do |node, ns|
+            node << XML::Node.new('id', name, ns)
+
+            options[:auth_info] ||= options.delete(:authInfo) || SecureRandom.hex(8).upcase
+            options.delete(:authInfo) # Make sure its not present
+
+            [:postal_info, :voice, :fax, :email, :auth_info, :disclose].each do |key|
+              value = options[key]
+              next if value.nil?
+
+              case key
+              when :postal_info
+                node << XML::Node.new('postalInfo', nil, ns).tap do |post|
+                  post['type'] = 'loc'
+                  create_contact_postal_info(post, value, ns)
+                end
+              when :disclose
+                node << XML::Node.new('disclose', nil, ns).tap do |d|
+                  d["flag"] = "1"
+                  Array(value).each do |v|
+                    d << XML::Node.new(v, ns)
+                  end
+                end
+              when :auth_info
+                node << XML::Node.new('authInfo', nil, ns).tap do |n|
+                  n << XML::Node.new('pw', value, ns)
+                end
+              when :voice, :fax, :email
+                name = key.to_s.gsub('_', '-')
+                node << XML::Node.new(name, value, ns)
+              end
+            end
+          end
+
+          unless extensions.empty?
+            extension << contact_ext('create') do |node, ns|
+              CONTACT_EXTENSION_KEYS.each do |key|
+                value = extensions[key]
+                next if value.nil?
+
+                case key
+                when :opt_out
+                  node << XML::Node.new('opt-out', value ? 'Y' : 'N', ns)
+                when :trad_name, :type, :co_no
+                  name = key.to_s.gsub('_','-')
+                  node << XML::Node.new(name, value, ns)
+                end
+              end
+            end
           end
         end
 
-        # Collects create contact information
-        #
-        # @param [XML::Node] creData XML
-        # @return [Hash]
-        def created_contact(creData)
-          { :roid => node_value(creData, 'contact:roid'),
-            :name => node_value(creData, 'contact:name') }
+        # @param [XML::Node] node XML Node to add data to
+        # @param [Hash] data Information to add to +node+
+        # @param [XML::Namespace] ns XML Namespace to create new nodes under
+        def create_contact_postal_info(node, data, ns)
+          [:name, :org, :addr].each do |key|
+            value = data[key]
+            next if value.nil?
+
+            case key
+            when :addr
+              node << XML::Node.new('addr', nil, ns).tap do |n|
+                [:street, :city, :sp, :pc, :cc].each do |k|
+                  v = value[k]
+                  next if v.nil?
+
+                  case v
+                  when Array
+                    v.each do |i|
+                      n << XML::Node.new(k, i, ns)
+                    end
+                  else
+                    n << XML::Node.new(k, v, ns)
+                  end
+                end
+              end
+            when :name, :org
+              name = key.to_s.gsub('_','-')
+              node << XML::Node.new(name, value, ns)
+            end
+          end
+        end
+
+        def create_contact_response(resp)
+          creData = resp.data.find('//contact:creData', namespaces).first
+          { :name => node_value(creData, 'contact:id'),
+            :crDate => Time.parse(node_value(creData, 'contact:crDate')) }
+        end
+
+        def create_host(create, extension, name, options)
+          create << host('create') do |node, ns|
+            node << XML::Node.new('name', name, ns);
+
+            options.each do |key, value|
+              case key
+              when :v4, :v6
+                node << XML::Node.new('addr', value, ns).tap do |addr|
+                  addr['ip'] = key.to_s
+                end
+              end
+            end
+          end
+        end
+
+        def create_host_response(resp)
+          creData = resp.data.find('//host:creData', namespaces).first
+          { :name => node_value(creData, 'host:name'),
+            :crDate => Time.parse(node_value(creData, 'host:crDate')) }
         end
     end
   end
