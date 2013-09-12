0216cca2fc0ad6fbc5c6311af9ed03c15fff851a
Geoff Garside <geoff@geoffgarside.co.uk>
2013-09-12 14:57:32 +0100

    Improve Domain::Check request tests

--- test/requests/domain/test_domain_check_request.rb
+++ test/requests/domain/test_domain_check_request.rb
@@ -5,10 +5,21 @@ class TestNominetDomainCheckRequest < Test::Unit::TestCase
     setup do
       @request = NominetEPP::Domain::Check.new('example.co.uk', 'example.me.uk')
       @xml     = prepare_request.to_xml
+
+      namespaces_from_request
     end
 
     should 'validate against schema' do
       assert @xml.validate_schema(schema)
+    end
+
+    should 'include names' do
+      names = []
+      xpath_each('//domain:name') do |node|
+        names << node.content.strip
+      end
+
+      assert_equal %w(example.co.uk example.me.uk), names
     end
   end
 end
