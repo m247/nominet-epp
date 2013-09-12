34be6a210342e1bcd416602cc7a12c91f2dfe300
Geoff Garside <geoff@geoffgarside.co.uk>
2013-09-12 14:57:43 +0100

    Improve Domain::Delete request tests

--- test/requests/domain/test_domain_delete_request.rb
+++ test/requests/domain/test_domain_delete_request.rb
@@ -5,10 +5,16 @@ class TestNominetDomainDeleteRequest < Test::Unit::TestCase
     setup do
       @request = NominetEPP::Domain::Delete.new('example.co.uk')
       @xml     = prepare_request.to_xml
+
+      namespaces_from_request
     end
 
     should 'validate against schema' do
       assert @xml.validate_schema(schema)
+    end
+    
+    should 'have name' do
+      assert_equal 'example.co.uk', xpath_find('//domain:name')
     end
   end
 end
