3a1dab88a2a239c64dec04d2fb04a5930079a7bd
Geoff Garside <geoff@geoffgarside.co.uk>
2014-10-16 09:02:56 +0100

    Remove 120m handling in favour of epp-client

--- test/requests/domain/test_domain_create_request.rb
+++ test/requests/domain/test_domain_create_request.rb
@@ -167,36 +167,13 @@ class TestNominetDomainCreateRequest < Test::Unit::TestCase
         end
       end
       context 'period = 120m' do
-        setup do
-          @time    = Time.now.utc
-          @request = NominetEPP::Domain::Create.new('example.co.uk',
-            :period => '120m', :registrant => 'UK-2349723',
-            :nameservers => %w(ns1.test.host ns2.test.host),
-            :auth_info => {:pw => '2381728348'})
-          @xml     = prepare_request.to_xml
-
-          namespaces_from_request
-        end
-
-        should 'validate against schema' do
-          assert @xml.validate_schema(schema)
-        end
-
-        should 'have create element' do
-          assert xpath_exists?('//domain:create'), "should have create element"
-        end
-
-        should 'have name element' do
-          assert_equal 'example.co.uk', xpath_find('//domain:name')
-        end
-        
-        should 'have period elemement of 10y' do
-          assert_equal '10', xpath_find('//domain:period')
-          assert_equal 'y', xpath_find('//domain:period/@unit')
-        end
-
-        should 'not have extension element' do
-          assert !xpath_exists?('//epp:extension'), "should not have extension element"
+        should 'raise argument error' do
+          assert_raises ArgumentError do
+            NominetEPP::Domain::Create.new('example.co.uk',
+              :period => '120m', :registrant => 'UK-2349723',
+              :nameservers => %w(ns1.test.host ns2.test.host),
+              :auth_info => {:pw => '2381728348'})
+          end
         end
       end
     end
--- test/requests/domain/test_domain_renew_request.rb
+++ test/requests/domain/test_domain_renew_request.rb
@@ -2,14 +2,41 @@ require 'helper'
 
 class TestNominetDomainRenewRequest < Test::Unit::TestCase
   context 'NominetEPP::Domain::Renew' do
-    setup do
-      @time    = Time.now.utc
-      @request = NominetEPP::Domain::Renew.new('example.co.uk', @time, '2y')
-      @xml     = prepare_request.to_xml
-    end
+    context 'normal renewal' do
+      setup do
+        @time    = Time.now.utc
+        @request = NominetEPP::Domain::Renew.new('example.co.uk', @time, '2y')
+        @xml     = prepare_request.to_xml
+      end
 
-    should 'validate against schema' do
-      assert @xml.validate_schema(schema)
+      should 'validate against schema' do
+        assert @xml.validate_schema(schema)
+      end
+    end
+    context 'period = 10y' do
+      setup do
+        @time    = Time.now.utc
+        @request = NominetEPP::Domain::Renew.new('example.co.uk', @time, '10y')
+        @xml     = prepare_request.to_xml
+        
+        namespaces_from_request
+      end
+
+      should 'validate against schema' do
+        assert @xml.validate_schema(schema)
+      end
+      
+      should 'have period elemement of 10y' do
+        assert_equal '10', xpath_find('//domain:period')
+        assert_equal 'y', xpath_find('//domain:period/@unit')
+      end
+    end
+    context 'period = 120m' do
+      should 'raise ArgumentError' do
+        assert_raises ArgumentError do
+          NominetEPP::Domain::Renew.new('example.co.uk', Time.now, '120m')
+        end
+      end
     end
   end
 end
